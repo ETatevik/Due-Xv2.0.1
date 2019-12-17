b66e81b11b42d59f75f4fe794f8432b38cd4d98c
eTatev <barev.tatevik@gmail.com>
2019-12-17 04:11:45 +0400

    End Scrolling, mabey and ;)

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
 jQuery(document).ready(function($){
 
+	// scroll
+	{
+		$("html").easeScroll({
+            frameRate: 60,
+            animationTime: 3000,
+            stepSize: 120,
+            pulseAlgorithm: 1,
+            pulseScale: 8,
+            pulseNormalize: 1,
+            accelerationDelta: 20,
+            accelerationMax: 1,
+            keyboardSupport: true,
+            arrowScroll: 50,
+            touchpadSupport: true,
+            fixedBackground: true
+        });
+	}
+
     //Header slider-------------------------------------------------------
     {
         let sliderImgages = [
